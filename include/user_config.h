2eac1ec71255b45b9c87b2ea61e3e1bff5e39143
Alexander Shelemba <ash089@yandex.ru>
2016-05-18 09:46:39 +0400

    2nd

---
+++ include/user_config.h
@@ -0,0 +1,7 @@
+#ifndef __USER_CONFIG_H__
+#define __USER_CONFIG_H__
+
+#define DEBUG_MODE	1
+
+
+#endif
--- user/user_main.c
+++ user/user_main.c
@@ -10,17 +10,11 @@
 *******************************************************************************/
 #include "ets_sys.h"
 #include "osapi.h"
-
 #include "user_interface.h"
 
+void user_rf_pre_init(void){}
 
 
-#include "user_devicefind.h"
-#include "user_webserver.h"
-
-void user_rf_pre_init(void)
-{
-}
 
 /******************************************************************************
  * FunctionName : user_init
@@ -30,22 +24,17 @@ void user_rf_pre_init(void)
 *******************************************************************************/
 void user_init(void)
 {
-    os_printf("SDK version:%s\n", system_get_sdk_version());
-
+#if DEBUG_MODE
+    os_printf("SDK version is :%s\n", system_get_sdk_version());
+#endif
     /*Establish a udp socket to receive local device detect info.*/
     /*Listen to the port 1025, as well as udp broadcast.
     /*If receive a string of device_find_request, it rely its IP address and MAC.*/
-    user_devicefind_init();
+    //user_devicefind_init();
 
     /*Establish a TCP server for http(with JSON) POST or GET command to communicate with the device.*/
     /*You can find the command in "2B-SDK-Espressif IoT Demo.pdf" to see the details.*/
-    /*the JSON command for curl is like:*/
-    /*3 Channel mode: curl -X POST -H "Content-Type:application/json" -d "{\"period\":1000,\"rgb\":{\"red\":16000,\"green\":16000,\"blue\":16000}}" http://192.168.4.1/config?command=light      */
-    /*5 Channel mode: curl -X POST -H "Content-Type:application/json" -d "{\"period\":1000,\"rgb\":{\"red\":16000,\"green\":16000,\"blue\":16000,\"cwhite\":3000,\"wwhite\",3000}}" http://192.168.4.1/config?command=light      */
-#ifdef SERVER_SSL_ENABLE
-    user_webserver_init(SERVER_SSL_PORT);
-#else
-    user_webserver_init(SERVER_PORT);
-#endif
+
+
 }
 
